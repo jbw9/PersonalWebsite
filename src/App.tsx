b1a6f94c8cc8527a5edd2a475e6fc2c69fb57225
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-09-26 00:28:40 -0500

    added dock

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { FloatingDock } from "./components/floating-dock";
+import { IconHome, IconSearch, IconBell, IconUser } from "@tabler/icons-react";
 
 function App() {
+  const dockItems = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/",
+    },
+    {
+      title: "Search",
+      icon: (
+        <IconSearch className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/search",
+    },
+    {
+      title: "Notifications",
+      icon: (
+        <IconBell className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/notifications",
+    },
+    {
+      title: "Profile",
+      icon: (
+        <IconUser className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/profile",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black w-full min-h-screen flex flex-col">
+      <FloatingDock
+        items={dockItems}
+        desktopClassName="fixed bottom-4 left-1/2 -translate-x-1/2"
+        mobileClassName="fixed bottom-4 right-4"
+      />
     </div>
   );
 }

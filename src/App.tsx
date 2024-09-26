942be86946ee1f6dd60de4bb2ab125fdf2311808
Jonathan Bernard Widjajakusuma <jbw7@illinois.edu>
2024-09-26 00:34:53 -0500

    added multistep loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { FloatingDock } from "./components/floating-dock";
 import { IconHome, IconSearch, IconBell, IconUser } from "@tabler/icons-react";
+import { MultiStepLoader } from "./components/multi-step-loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const dockItems = [
     {
       title: "Home",
@@ -36,13 +47,28 @@ function App() {
     },
   ];
 
+  const loadingStates = [
+    { text: "Importing Jonathan's information..." },
+    { text: "Fetching latest projects..." },
+    { text: "Downloading app previews..." },
+    { text: "Almost there..." },
+  ];
+
   return (
     <div className="bg-black w-full min-h-screen flex flex-col">
-      <FloatingDock
-        items={dockItems}
-        desktopClassName="fixed bottom-4 left-1/2 -translate-x-1/2"
-        mobileClassName="fixed bottom-4 right-4"
+      <MultiStepLoader
+        loadingStates={loadingStates}
+        loading={loading}
+        duration={1000}
+        loop={true}
       />
+      {!loading && (
+        <FloatingDock
+          items={dockItems}
+          desktopClassName="fixed bottom-4 left-1/2 -translate-x-1/2"
+          mobileClassName="fixed bottom-4 right-4"
+        />
+      )}
     </div>
   );
 }
